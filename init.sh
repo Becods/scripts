d739f940b6fcaac78f6a18107c97f0d50c03de78
蓝云Reyes <i@muir.fun>
2022-06-19 17:47:56 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -17,17 +17,17 @@ read -p "按任意键继续 Ctrl+C退出"
 echo -e "$Info 正在禁用ipv6"
 sed -i 's/#precedence ::ffff:0:0\/96  100/precedence ::ffff:0:0\/96  100/' /etc/gai.conf
 
-echo "$Info 正在替换源"
+echo -e "$Info 正在替换源"
 cat >/etc/apt/sources.list <<EOF
 deb http://mirrors.cloud.tencent.com/debian bullseye main contrib
 deb http://mirrors.cloud.tencent.com/debian bullseye-updates main contrib
 deb http://mirrors.cloud.tencent.com/debian-security bullseye-security main contrib
 EOF
 
-echo "$Info 正在更新源"
-apt-get update
+echo -e "$Info 正在更新源"
+apt-get -qq update 2>/dev/null >/dev/null
 
-echo "$Info 正在修改日志最大大小"
+echo -e "$Info 正在修改日志最大大小"
 echo "HISTSIZE=100000" >> /etc/profile
 
 cat >>/etc/bashrc <<EOF
@@ -42,6 +42,6 @@ alias mv='mv -i'
 PS1='\[\e[37m\][\[\e[32m\]\u\[\e[37m\]@\h \[\e[36m\]\W\[\e[0m\]]\$ '
 EOF
 
-echo "$Info 修改完成，3秒后重启系统"
+echo -e "$Info 修改完成，3秒后重启系统"
 sleep 3
 reboot
